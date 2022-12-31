8343b79ce25522e224e74924734a2f33ddd30e72
Kacper Doga <doga.kacper@gmail.com>
2022-12-31 01:39:58 +0100

    add register redirection, rename fx ids

--- src/main/java/dev/varev/managementsystem/controller/LoginController.java
+++ src/main/java/dev/varev/managementsystem/controller/LoginController.java
@@ -1,29 +1,41 @@
 package dev.varev.managementsystem.controller;
 
+import dev.varev.managementsystem.Main;
+import dev.varev.managementsystem.service.LoginService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.time.Duration;
+
 public class LoginController {
+
+    private LoginService loginService;
 
     @FXML
     private Label welcomeText;
     @FXML
-    private TextField usernameInput;
+    private TextField username;
     @FXML
-    private PasswordField passwordInput;
+    private PasswordField password;
     @FXML
     private Button loginButton;
     @FXML
     private Label notification;
     @FXML
-    private Label registerLink;
+    private Label registerRedirection;
 
     @FXML
     protected void handleLogin() {
         welcomeText.setText("Logging in...");
+    }
+
+    @FXML
+    protected void redirectToRegister() {
+        welcomeText.setText("Redirecting...");
+        Main.changeSceneDelayed("register", Duration.ofMillis(1000));
     }
 
 }
