6e77f9929ea8fc455727f2abfd019f194618b0fd
Kacper Doga <doga.kacper@gmail.com>
2022-12-31 01:38:44 +0100

    create scene manager

--- src/main/java/dev/varev/managementsystem/Main.java
+++ src/main/java/dev/varev/managementsystem/Main.java
@@ -1,16 +1,26 @@
 package dev.varev.managementsystem;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main extends Application {
 
+    private static Stage stage;
+
     @Override
     public void start(Stage stage) throws IOException {
+        Main.stage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setWidth(1024);
@@ -18,6 +28,26 @@ public class Main extends Application {
         stage.setTitle("Enterprise Resource Management");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void changeScene(String profile) {
+        profile += ".fxml";
+        Parent parent = null;
+
+        try {
+            parent = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource(profile)));
+        } catch (IOException ignore) {
+            /* TODO: Redirect to error page */
+        }
+
+        stage.getScene().setRoot(parent);
+    }
+
+    public static void changeSceneDelayed(String profile, Duration delay) {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(() -> {
+            changeScene(profile);
+        }, delay.toMillis(), 1, TimeUnit.MILLISECONDS);
     }
 
     public static void main(String[] args) {
